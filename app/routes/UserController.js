8401373a46216be8ceb44d21ad83d1c2ec9d215a
Jacopo Veronese (posky) <jacopo.veronese@studenti.unitn.it>
2024-05-05 15:55:44 +0200

    Minor changes in Registration endpoint
    Add try and catch block in Registration endpoint

--- app/routes/UserController.js
+++ app/routes/UserController.js
@@ -10,26 +10,26 @@ const EXPIRAL_AUTH_TIME = "2 days";
 
 // REGISTRATION
 router.post("", async (req, res) => {
-  // Validate the data
-  const { error } = registerValidation(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  // Check if email exists
-  const savedEmail = await User.findOne({ email: req.body.email });
-  if (savedEmail) return res.status(400).send("Email already exists");
-
-  // Check if username exists
-  const savedUsername = await User.findOne({ username: req.body.username });
-  if (savedUsername) return res.status(400).send("Username already exists");
-
-  const user = new User({
-    name: req.body.name,
-    surname: req.body.surname,
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-  });
   try {
+    // Validate the data
+    const { error } = registerValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    // Check if email exists
+    const savedEmail = await User.findOne({ email: req.body.email });
+    if (savedEmail) return res.status(400).send("Email already exists");
+
+    // Check if username exists
+    const savedUsername = await User.findOne({ username: req.body.username });
+    if (savedUsername) return res.status(400).send("Username already exists");
+
+    const user = new User({
+      name: req.body.name,
+      surname: req.body.surname,
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
     const savedUser = await user.save();
     res.send(savedUser);
   } catch (err) {
