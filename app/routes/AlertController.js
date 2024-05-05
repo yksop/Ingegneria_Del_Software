33bcb42cadbb880bd9c44267eaeb3aa376b1cb23
Federica_Lorenzini <kikalore2002@gmail.com>
2024-05-05 10:32:02 +0200

    Modified AlertController

--- app/routes/AlertController.js
+++ app/routes/AlertController.js
@@ -1,4 +1,18 @@
 const router = require("express").Router();
 const User = require("../models/User");
 
+//Post alert into DB
+
+
+//get alert from DB
+router.get('/:id', async(req,res) => { 
+	let alert = await Alert.findById(req.params.id);
+    if(!alert) res.status(400).send("Alert does not exist\n");
+	res.status(200).json({
+		self: '/api/v1/alerts/' + alert.id,
+		title: book.title
+    })
+});
+
+
 module.exports = router;
