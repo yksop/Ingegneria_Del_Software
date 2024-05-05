b6f522e6788edfe5d51f94ac48ffa6ea06b3a63d
AlbertoDB20 <alberto.dalbosco@studenti.unitn.it>
2024-05-05 22:42:42 +0200

    Finished function to create and save Alert

--- app/routes/AlertController.js
+++ app/routes/AlertController.js
@@ -2,32 +2,58 @@ const router = require("express").Router();
 const User = require("../models/User");
 const Alert = require("../models/Alert");
 const mongoose = require("mongoose");
+const { alertValidation } = require("../../validation");
 
 
 // Create and save new Alert in the DB
 router.post("", async (req, res) => {
-  // Create a new Alert
-  const alert = new Alert({
-    latitude: req.body.latitude,
-    longitude: req.body.longitude,
-    type: req.body.type,
-    description: req.body.description,
-    radius: req.body.radius,
-    expiresIn: req.body.expiresIn,
-    isActive: req.body.isActive,
-    timeForAmbulance: req.body.timeForAmbulance,
-  });
-  
   try {
-    // Save the Alert in the DB 
-    const savedAlert = await alert.save();
+    // Validate the data before creating a new Alert
+    const { error } = alertValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    // Create a new Alert
+    const newAlert = new Alert({
+      latitude: req.body.latitude,
+      longitude: req.body.longitude,
+      triage: req.body.triage,
+      radius: req.body.radius,
+      expiresIn: req.body.expiresIn,
+      isActive: req.body.isActive,
+      description: req.body.description,
+      timeForAmbulance: req.body.timeForAmbulance,
+    });
+
+    // If description is not provided, set it to an empty string
+    if (!newAlert.description) newAlert.description = "";
+
+    // If timeForAmbulance is not provided, set it to 0
+    if (!newAlert.timeForAmbulance) newAlert.timeForAmbulance = 0;
+
+    // Check if there is an identical active Alert in the DB
+    const identicalAlert = await Alert.findOne({
+      latitude: req.body.latitude,
+      longitude: req.body.longitude,
+      triage: req.body.triage,
+      radius: req.body.radius,
+      expiresIn: req.body.expiresIn,
+      isActive: true,
+      description: req.body.description,
+      timeForAmbulance: req.body.timeForAmbulance,
+    });
+
+    // If there is an identical active Alert in the DB, return an error
+    if (identicalAlert) return res.status(400).send("Identical active Alert already exists");
+    
+    // Save the Alert in the DB
+    const savedAlert = await newAlert.save();
     console.log("Alert saved in the DB");
-    res.send(savedAlert);
+    return res.send(savedAlert);
   } catch (err) {
     // print the error in the console
     console.log("Error in saving the Alert in the DB: " + err);
     // send the error in the response
-    res.status(400).send(err);
+    return res.status(400).send(err);
   }
 });
 
