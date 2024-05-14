6216b4b38c588c5e059639740819fbd5ffb05490
Federica_Lorenzini <kikalore2002@gmail.com>
2024-05-14 16:22:04 +0200

    Modified header for reserved area

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,24 +1,35 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import Header from './components/Header.vue'
-import Footer from './components/Footer.vue'
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+import Header from "./components/Header.vue";
+import HeaderReservedArea from "./components/HeaderReservedArea.vue";
+import Footer from "./components/Footer.vue";
+
+const route = useRoute();
+
+const reservedRoutes = ["/logged", "/upgradeDowngrade"];
+const headerComponent = computed(() => {
+  return reservedRoutes.includes(route.path) ? HeaderReservedArea : Header;
+});
 </script>
 
-
 <template>
-<body>
-  <div class="container">
-    <header>
-      <Header msg="VTC: Volontari Trentini Connessi" />
-    </header>
+  <body>
+    <div class="container">
+      <header>
+        <component
+          :is="headerComponent"
+          msg="VTC: Volontari Trentini Connessi"
+        />
+      </header>
 
-    <main>
-      <RouterView />
-    </main>
+      <main>
+        <RouterView />
+      </main>
 
-    <footer>
-      <Footer />
-    </footer>
-  </div>
-</body>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  </body>
 </template>
---
+++ frontend/src/components/HeaderReservedArea.vue
@@ -0,0 +1,121 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="head">
+    <div class="header-content">
+      <div class="header-group left">
+        <a href="https://cri.it" class="image-with-link">
+          <img
+            src="@/assets/emblema_CRI.png"
+            alt="Left Image"
+            class="img-left"
+          />
+          <!-- Only one image on the left -->
+        </a>
+      </div>
+      <div class="header-logo">
+        <img
+          src="@/assets/logo_final.jpg"
+          alt="Website Logo"
+          class="logo-img"
+        />
+      </div>
+      <div class="header-group right">
+        <a href="https://www.comune.trento.it" class="image-with-link">
+          <img
+            src="@/assets/comune-di-Trento.jpg"
+            alt="Right Image 1"
+            class="small-img"
+          />
+        </a>
+        <a href="https://www.unitn.it" class="image-with-link">
+          <img src="@/assets/unitn.jpg" alt="Right Image 2" class="small-img" />
+        </a>
+      </div>
+    </div>
+    <!-- <nav class="navbar">
+      <ul>
+        <li>
+          <RouterLink to="/about">About</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/registration">Registration</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/login">Login</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/history">History</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/contact">Contact</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/profile">Profile</RouterLink>
+        </li>
+      </ul>
+    </nav> -->
+  </div>
+</template>
+
+
+<style scoped>
+.head {
+  background-color: white;
+  color: #fff;
+}
+
+.header-content {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+}
+
+.header-group {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+.small-img {
+  width: relative; /* Adjust width as needed */
+  height: 100px; /* Adjust height as needed */
+}
+
+.img-left {
+  width: relative;
+  height: 200px;
+}
+
+.logo-img {
+  max-width: 100%;
+  height: auto;
+  transition: width 0.5s ease; /* Smooth transition for logo resize */
+}
+
+.header-logo {
+  flex: 1;
+  text-align: center;
+  max-width: 500px; /* Maximum logo width */
+}
+
+.image-with-link:hover {
+  transform: scale(1.1);
+  transition: 0.5s;
+}
+
+@media (max-width: 768px) {
+  .logo-img {
+    height: 80%; /* Smaller logo on smaller screens */
+  }
+}
+</style>
