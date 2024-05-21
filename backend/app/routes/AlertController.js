09628b3c9cc1497b0e3cd3010f086c1b6906fa97
Jacopo Veronese (posky) <jacopo.veronese@studenti.unitn.it>
2024-05-21 18:51:19 +0200

    Add authMiddleware's CallBack function

--- backend/app/routes/AlertController.js
+++ backend/app/routes/AlertController.js
@@ -59,32 +59,39 @@ router.post("", async (req, res) => {
 });
 
 // RETIRE ALERT
-router.put("/:id", verifyToken, async (req, res) => {
-  try {
-    if (!req.params.id) return res.status(400).send("Alert ID is required\n");
+router.put(
+  "/:id",
+  verifyToken((authData) => {
+    if (authData.isOperator118) return true;
+    return false;
+  }),
+  async (req, res) => {
+    try {
+      if (!req.params.id) return res.status(400).send("Alert ID is required\n");
 
-    if (mongoose.Types.ObjectId.isValid(req.params.id) === false)
-      return res.status(400).send("Invalid Alert ID\n");
+      if (mongoose.Types.ObjectId.isValid(req.params.id) === false)
+        return res.status(400).send("Invalid Alert ID\n");
 
-    const result = await Alert.updateOne(
-      { _id: req.params.id },
-      { isActive: false }
-    ).exec();
+      const result = await Alert.updateOne(
+        { _id: req.params.id },
+        { isActive: false }
+      ).exec();
 
-    if (result.matchedCount === 0)
-      return res.status(404).send("Alert not found, no updates were made\n");
+      if (result.matchedCount === 0)
+        return res.status(404).send("Alert not found, no updates were made\n");
 
-    if (result.modifiedCount === 0)
-      return res
-        .status(400)
-        .send("Alert is already non-active, no updates were made\n");
+      if (result.modifiedCount === 0)
+        return res
+          .status(400)
+          .send("Alert is already non-active, no updates were made\n");
 
-    return res.status(200).send("Alert updated successfully\n");
-  } catch (err) {
-    console.log(err);
-    res.status(400).send(err);
+      return res.status(200).send("Alert updated successfully\n");
+    } catch (err) {
+      console.log(err);
+      res.status(400).send(err);
+    }
   }
-});
+);
 
 // Get alert from DB
 router.get("/:id", async (req, res) => {
