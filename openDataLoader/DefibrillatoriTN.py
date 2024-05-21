0c23d2b9144de177ff8cd48e52862a0f8cf380d2
AlbertoDB20 <alberto.dalbosco@studenti.unitn.it>
2024-05-21 09:21:06 +0200

    Added defibrillatori database and script to create dataset

---
+++ openDataLoader/DefibrillatoriTN.py
@@ -0,0 +1,55 @@
+import pandas as pd
+from pymongo import MongoClient             # pip install pandas pymongo
+import re
+
+
+# Stringa di connessione MongoDB
+mongo_connection_string = 'mongodb+srv://albertodalbosco:vale@ingegneriadelsoftware.rklgpcl.mongodb.net/?retryWrites=true&w=majority&appName=IngegneriaDelSoftware'
+
+# Nome del database e della collezione
+mongo_db_name = 'test'
+mongo_collection_name = 'defibrillatori'
+
+# Connessione a MongoDB
+try:
+    client = MongoClient(mongo_connection_string)
+    db = client[mongo_db_name]
+    collection = db[mongo_collection_name]
+    print("Connessione riuscita")
+except Exception as e:
+    print(f"Errore di connessione: {e}")
+    exit(1)
+
+# Read csv file
+csv_file_path = 'openDataLoader/data/defibrillatori.csv'
+
+df = pd.read_csv(csv_file_path, sep=';')  # Assicurati di usare il separatore corretto (qui uso tab)
+
+# Funzione per estrarre le coordinate dalla colonna WKT
+def extract_coordinates(wkt):
+    match = re.match(r'POINT \(([^ ]+) ([^ ]+)\)', wkt)
+    if match:
+        return float(match.group(1)), float(match.group(2))
+    return None, None
+
+# Estrazione e stampa dei dati
+data = []
+for index, row in df.iterrows():
+    lat, lon = extract_coordinates(row['WKT'])
+    record = {
+        'latitudine': lat,
+        'longitudine': lon,
+        'id': row['id'],
+        'codvia': row['codvia'],
+        'desvia': row['desvia'],
+        'fumetto': row['fumetto']
+    }
+    data.append(record)
+    print(f"Latitudine: {lat}, Longitudine: {lon}, id: {row['id']}, codvia: {row['codvia']}, desvia: {row['desvia']}, fumetto: {row['fumetto']}")
+
+# Inserimento dei dati in MongoDB
+try:
+    collection.insert_many(data)
+    print(f'Inseriti {len(data)} documenti in MongoDB nella collezione "{mongo_collection_name}".')
+except Exception as e:
+    print(f"Errore durante l'inserimento dei dati: {e}")
