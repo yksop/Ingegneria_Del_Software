16f015c77cc65c3dcfd25259de97a8f8cf92d1c6
AlbertoDB20 <alberto.dalbosco@studenti.unitn.it>
2024-05-22 09:30:10 +0200

    Resolved bug in map

--- frontend/src/views/HomePageView.vue
+++ frontend/src/views/HomePageView.vue
@@ -126,7 +126,7 @@ export default {
       }
 
       if (this.selectedMap === 'osm') {
-        this.tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        this.tileLayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
           attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         });
       } else if (this.selectedMap === 'otm') {
