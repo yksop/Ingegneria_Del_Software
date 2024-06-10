8006b01a575e0db1cc80b3304897f3ab6b24dc91
Federica_Lorenzini <kikalore2002@gmail.com>
2024-06-10 14:12:41 +0200

    AlertController testing: authorization management

--- backend/__tests__/AlertController.test.js
+++ backend/__tests__/AlertController.test.js
@@ -1,39 +1,68 @@
-const request = require("supertest");
 const mongoose = require("mongoose");
 const faker = require("faker");
-const url = "localhost:3000/api/v1/alerts";
 require("dotenv").config();
+const request = require("supertest");
+const url = "http://localhost:3000";
 
-jest.setTimeout(30000);
+describe("User with specific roles", () => {
+  let authToken = "";
 
-
-describe("GET /api/v1/alerts/:id", () => {
-  test("GET /api/v1/alerts/:id with a valid alert ID should return the alert", async () => {
-    const response = await request(url)
-      .get("/:id")
+  beforeAll(async () => {
+    //Create User with permisisons
+    await request(url)
+      .post("/api/v1/users")
+      .send({
+        name: "John",
+        surname: "Doe",
+        username: faker.internet.userName(),
+        email: faker.internet.email(),
+        password: "Password123!",
+        latitude: 12.34,
+        longitude: 56.78,
+        volunteer: {
+          isVolunteer: true,
+        },
+        certifier: {
+          isCertifier: true,
+        },
+        operator118: {
+          isOperator118: true,
+        },
+      })
       .expect(200);
 
-    const { alert } = response.body;
+    //Log In the User with permissions
+    const loginResponse = await request(url)
+      .post("/api/v1/tokens")
+      .send({
+        username: "johndoe",
+        password: "Password123!",
+      })
+      .expect(200);
+    console.log("Token of user with permissions: ", loginResponse.body.token);
+    authToken = loginResponse.body.token;
+  });
+
+  test("GET /api/v1/alerts/:id with a valid alert ID should return the alert", async () => {
+    const response = await request(url)
+      .get("/api/v1/alerts/6666c22349f7eda44ee685b5")
+      .set("Authorization", `Bearer ${authToken}`)
+      .expect(200);
+    console.log(response.body);
+
+    const alert = response.body;
 
     expect(alert).toBeDefined();
     expect(typeof alert).toBe("object");
     expect(alert._id).toBeDefined();
     expect(alert.latitude).toBeDefined();
     expect(alert.longitude).toBeDefined();
-    return response;
-  });
-
-  test("GET /api/v1/alerts/:id with an invalid alert ID should return an error", async () => {
-    const response = await request(url)
-      .get("/:id")
-      .set("Authorization", "Bearer <valid_token>")
-      .expect(400);
-
-    const { error } = response.body;
-
-    expect(error).toBeDefined();
-    expect(typeof error).toBe("string");
-
-    return response;
+    expect(alert.triage).toBeDefined();
+    expect(alert.emergency).toBeDefined();
+    expect(alert.radius).toBeDefined();
+    expect(alert.expiresIn).toBeDefined();
+    expect(alert.isActive).toBeDefined();
+    expect(alert.description).toBeDefined();
+    expect(alert.timeForAmbulance).toBeDefined();
   });
 });
