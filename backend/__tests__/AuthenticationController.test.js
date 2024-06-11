301495073104c86726eadda95943048c363b7b04
Federica_Lorenzini <kikalore2002@gmail.com>
2024-06-11 13:46:48 +0200

    Fix testing on AuthenticationController

--- backend/__tests__/AuthenticationController.test.js
+++ backend/__tests__/AuthenticationController.test.js
@@ -1,16 +1,42 @@
 const request = require("supertest");
 const mongoose = require("mongoose");
 const faker = require("faker");
-const url = "localhost:3000/api/v1/tokens";
+const url = "localhost:3000/api/v1";
 require("dotenv").config();
 
 jest.setTimeout(30000);
-let myToken;
+let myToken = "";
+let userId = "";
 describe("POST /api/v1/tokens", () => {
-  
   beforeAll(async () => {
+    const registrationResponse = await request(url)
+      .post("/users")
+      .send({
+        name: "John",
+        surname: "Doe",
+        username: "johndoe",
+        email: "prova@gmail.com",
+        password: "Password123!",
+        latitude: 12.34,
+        longitude: 56.78,
+        volunteer: {
+          isVolunteer: false,
+        },
+        certifier: {
+          isCertifier: false,
+        },
+        operator118: {
+          isOperator118: false,
+        },
+      })
+      .set("Content-Type", "application/json")
+      .expect(200);
+    console.log("Response: ", registrationResponse.body);
+    userId = registrationResponse.body._id;
+    console.log("userId:", userId);
+
     const response = await request(url)
-      .post("")
+      .post("/tokens")
       .send({
         username: "johndoe",
         password: "Password123!",
@@ -18,11 +44,19 @@ describe("POST /api/v1/tokens", () => {
       .set("Content-Type", "application/json")
       .expect(200);
 
-    const { token } = response.body;
+    console.log(response.body.token);
+    const token = response.body.token;
+    console.log(token);
     myToken = token;
   });
 
-  test("Token should be defined and a string", () => {
+  afterAll(async () => {
+    return request(url)
+      .delete(`/users/${userId}`)
+      .set("Content-Type", "application/json")
+      .expect(200);
+  });
+  test("Token should be defined and a string", async () => {
     expect(myToken).toBeDefined();
     expect(typeof myToken).toBe("string");
   });
